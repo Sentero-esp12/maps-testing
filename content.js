74d98662ea1266efe3a17971883cd0523d485382
Sentero-esp12 <35060206+Sentero-esp12@users.noreply.github.com>
2020-10-13 19:39:14 +0300

    update

--- content.js
+++ content.js
@@ -1,20 +1,3 @@
-/* let jquery = document.createElement('script');
-jquery.src = chrome.runtime.getURL('jquery.js');
-(document.head || document.documentElement).appendChild(jquery); */
-
-
 let windowScript = document.createElement('script');
 windowScript.src = chrome.runtime.getURL('script.js');
 (document.head || document.documentElement).appendChild(windowScript);
-
-/* document.addEventListener('keypress', function (e) {
-   if (e.key === 'w') {
-      //throw new Error(1)
-   }
-}); */
-
-/* chrome.extension.onMessage.addListener(function (response, sender, sendResponse) {
-
-}) */
-
-
